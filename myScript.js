6d01a179b7cc666288b7e942dfad362b75ed2ca2
qkr10 <qkrtngks10@naver.com>
2019-09-25 17:38:57 +0900

    error correct
    linting by JSHint

--- myScript.js
+++ myScript.js
@@ -1,8 +1,8 @@
-var h1Ele, textNode, bodyEle
+var h1Ele, textNode, bodyEle;
 window.onload = function() {
-    h1Ele = document.createElement('h1')
-    textNode = document.createTextNode('asdf')
-    bodyEle = document.getElementById('space')
-    h1Ele.appendChild(textNode)
-    bodyEle.appendChild(h1Ele)
-}
+    h1Ele = document.createElement('h1');
+    textNode = document.createTextNode('asdf');
+    bodyEle = document.getElementById('space');
+    h1Ele.appendChild(textNode);
+    bodyEle.appendChild(h1Ele);
+};
